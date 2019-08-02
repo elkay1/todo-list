fc625ce7d11a5d69dadf7597a7cbfe139f22b941
Liam Kinson <liamkinson@Liams-MacBook-Pro.local>
2019-08-02 10:06:19 +1000

    First commit, styling mostly done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { WhiteboardComponent } from "./components/whiteboard/whiteboard.component";
+import { AddTodoComponent } from './components/add-todo/add-todo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, WhiteboardComponent, AddTodoComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
